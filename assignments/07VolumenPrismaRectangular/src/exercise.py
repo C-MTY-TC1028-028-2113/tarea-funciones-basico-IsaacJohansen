6ffd6018a6329b0aecc98147026ab3d97c6ecac5
Isaac Johansen <A00832598@itesm.mx>
2021-08-26 09:59:32 -0500

    Trea 1

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,5 +1,9 @@
 # Escribe aquí tus funciones...
+def rectangulo(lado1,lado2):
+    return lado1*lado2
 
+def volumen_prisma(x,y,z):
+    return rectangulo(x,y)*z
 def main():
     #escribe tu código abajo de esta línea
     b = float(input("Dame la base: "))
